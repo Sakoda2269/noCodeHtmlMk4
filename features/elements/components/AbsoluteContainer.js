5d0fd7eae85f3f913aa6af6547506d6df70a8215
Sakoda2269 <ksakoda.2269@gmail.com>
2024-10-29 14:48:43 +0900

    はみ出しを消すことを追加

--- features/elements/components/AbsoluteContainer.js
+++ features/elements/components/AbsoluteContainer.js
@@ -5,7 +5,7 @@ export default function AbsoluteContainer({ element, path }) {
 
 
     return (
-        <div style={{width: "100%", height: "100%", border: "1px solid black"}}>
+        <div style={{width: "100%", height: "100%", border: "1px solid black", position: "relative", overflow: "hidden"}}>
             {element.children.map((value, index) => (
                 <Wrapper element={value} layout="absolute" key={index} path={`${path}/${index}`}/>
             ))}
