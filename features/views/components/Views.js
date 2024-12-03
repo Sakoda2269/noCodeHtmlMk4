189911e969d8e418b7e35987edd09438e6f3f55c
Sakoda2269 <ksakoda.2269@gmail.com>
2024-12-03 16:08:24 +0900

    タイポ修正

--- features/views/components/Views.js
+++ features/views/components/Views.js
@@ -3,7 +3,7 @@ import styles from "./Vies.module.css";
 export default function Views() {
     return (
         <div className={styles.container}>
-            <button className={styles.containerElement}>add Vies</button>
+            <button className={styles.containerElement}>add View</button>
         </div>
     )
 }
