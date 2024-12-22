aa6bf1a2c42533a3686490fd7a6976887fbb8394
Sakoda2269 <ksakoda.2269@gmail.com>
2024-12-22 23:17:07 +0900

    レイヤーを最初から閉じた状態に変更

--- features/constructUI/layer/components/Layer.js
+++ features/constructUI/layer/components/Layer.js
@@ -8,18 +8,18 @@ import { SetProjectContext } from "@/features/constructUI/project/contexts/proje
 export default function Layer() {
 
     const [project, dragging, setDragging, addScreen] = useLayer();
-    
+
     return (
         <LayerDragginContext.Provider value={dragging}>
             <SetLayerDraggingContext.Provider value={setDragging}>
-                <div style={{widht: "100%", height: "100%", padding: "10px", overflowY: "auto"}}>
+                <div style={{ widht: "100%", height: "100%", padding: "10px", overflowY: "auto" }}>
+                    <div style={{ textAlign: "center", padding: "10px" }}>
+                        <button style={{ width: "100%" }} onClick={addScreen}>add screen</button>
+                    </div>
                     <div className={styles.container}>
                         {project.screens.map((screen, index) => (
-                            <Component key={index} component={screen} depth={0} inIndex={index} path="" screenIndex={index}/>
+                            <Component key={index} component={screen} depth={0} inIndex={index} path="" screenIndex={index} />
                         ))}
-                    </div>
-                    <div style={{textAlign: "center", padding: "10px"}}>
-                        <button style={{width: "100%"}} onClick={addScreen}>add screen</button>
                     </div>
                 </div>
             </SetLayerDraggingContext.Provider>
@@ -27,88 +27,88 @@ export default function Layer() {
     )
 }
 
-function Component({component, depth, inIndex, path, screenIndex}) {
+function Component({ component, depth, inIndex, path, screenIndex }) {
 
-    const [isOpen, setOpen] = useState(true);
+    const [isOpen, setOpen] = useState(false);
 
     const children = depth == 0 ? component.components : component.children;
-    
+
     const [
-            select, dragStart, drop, dragOver, 
-            dragOn, dragEnter, dragLeave, buttomDragOn, 
-            buttomDragEnter, buttomDragLeave, isSelecting, buttonBorder
-        ] = useComponent(path, children, screenIndex);
-        
+        select, dragStart, drop, dragOver,
+        dragOn, dragEnter, dragLeave, buttomDragOn,
+        buttomDragEnter, buttomDragLeave, isSelecting, buttonBorder
+    ] = useComponent(path, children, screenIndex);
+
     const collapseOpen = () => {
-        if(isOpen && isSelecting) {
+        if (isOpen && isSelecting) {
             setOpen(false);
         } else {
             setOpen(true);
         }
     }
-    
+
     return (
         <div className={styles.component}>
-                {depth != 0 &&
-                    <div style={{width: "100%", height: "15px"}}
-                        className={`${dragOn ? styles.dragOn : ""}`} 
-                        onDragOver={dragOver}
-                        onDragEnter={dragEnter}
-                        onDragLeave={dragLeave}
-                        onDrop={(e) => drop(e, "u")}>
-                    </div>}
-                <div draggable="true" onDragStart={dragStart} style={{paddingBottom: "5px"}}>
-                    {depth == 0 ? (
-                        <button 
-                            className={styles.layerButton}
-                            style={{border: buttonBorder}}
-                            onClick={() => {
-                                collapseOpen();
-                                select();
-                            }}
-                            aria-controls="children"
-                            aria-expanded={isOpen}
-                        >
-                            {component.title}
-                        </button>
-                    ) : (
-                        <button 
-                            className={styles.layerButton}
-                            style={{border: buttonBorder}}
-                            onClick={() => {
-                                collapseOpen();
-                                select();
-                            }}
-                            aria-controls="children"
-                            aria-expanded={isOpen}
-                        >
-                            {component.data.id.value}
-                        </button>
-                    )}
-                    {children && <Collapse in={isOpen}>
-                        <div className={styles.container} id="children">
-                            {children.map((child, index) => (
-                                <Component 
-                                    key={index} 
-                                    component={child} 
-                                    depth={depth+1} 
-                                    inIndex={index} 
-                                    path={
-                                        depth == 0 ? index+"" : path + "/" + index
-                                    }
-                                    screenIndex={screenIndex}/>
-                                    
-                            ))}
-                            <div style={{width: "90%", height: "15px"}}
-                                className={`${buttomDragOn ? styles.dragOn : ""}`}
-                                onDragOver={dragOver}
-                                onDragEnter={buttomDragEnter}
-                                onDragLeave={buttomDragLeave}
-                                onDrop={(e) => drop(e, "d")}>
-                            </div>
+            {depth != 0 &&
+                <div style={{ width: "100%", height: "15px" }}
+                    className={`${dragOn ? styles.dragOn : ""}`}
+                    onDragOver={dragOver}
+                    onDragEnter={dragEnter}
+                    onDragLeave={dragLeave}
+                    onDrop={(e) => drop(e, "u")}>
+                </div>}
+            <div draggable="true" onDragStart={dragStart} style={{ paddingBottom: "5px" }}>
+                {depth == 0 ? (
+                    <button
+                        className={styles.layerButton}
+                        style={{ border: buttonBorder }}
+                        onClick={() => {
+                            collapseOpen();
+                            select();
+                        }}
+                        aria-controls="children"
+                        aria-expanded={isOpen}
+                    >
+                        {component.title}
+                    </button>
+                ) : (
+                    <button
+                        className={styles.layerButton}
+                        style={{ border: buttonBorder }}
+                        onClick={() => {
+                            collapseOpen();
+                            select();
+                        }}
+                        aria-controls="children"
+                        aria-expanded={isOpen}
+                    >
+                        {component.data.id.value}
+                    </button>
+                )}
+                {children && <Collapse in={isOpen}>
+                    <div className={styles.container} id="children">
+                        {children.map((child, index) => (
+                            <Component
+                                key={index}
+                                component={child}
+                                depth={depth + 1}
+                                inIndex={index}
+                                path={
+                                    depth == 0 ? index + "" : path + "/" + index
+                                }
+                                screenIndex={screenIndex} />
+
+                        ))}
+                        <div style={{ width: "90%", height: "15px" }}
+                            className={`${buttomDragOn ? styles.dragOn : ""}`}
+                            onDragOver={dragOver}
+                            onDragEnter={buttomDragEnter}
+                            onDragLeave={buttomDragLeave}
+                            onDrop={(e) => drop(e, "d")}>
                         </div>
-                    </Collapse>}
-                </div>
+                    </div>
+                </Collapse>}
+            </div>
         </div>
     )
 }
